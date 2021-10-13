40ebbebb24e2b6915283d7b88ec449d2298d352a
Robert <rcrooks@brightcove.com>
2021-10-13 13:31:12 -0400

    new translations

--- assets/js/feedback.js
+++ assets/js/feedback.js
@@ -1,5 +1,9 @@
+---
+layout: null
+---
+
 var fby = fby || [];
-fby.push(['showTab', {id: '', position: 'right', color: 'rgb(243, 148, 36)'}]);
+fby.push(['showTab', {id: '{{ feedbackify_id }}', position: 'right', color: '{{ site.product_color }}'}]);
 (function () {
     var f = document.createElement('script'); f.type = 'text/javascript'; f.async = true;
     f.src = '//cdn.feedbackify.com/f.js';
